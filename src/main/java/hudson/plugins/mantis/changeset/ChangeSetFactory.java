d26f0001736e129aced2911d6422699a97766a18
sogabe <sogabe@dev.java.net>
2010-04-04 05:30:42 +0000

    [FIXED HUDSON-6114]  - Updated wsdl to support Mantis 1.2.0.  -  bumped related plugins.

--- src/main/java/hudson/plugins/mantis/changeset/ChangeSetFactory.java
+++ src/main/java/hudson/plugins/mantis/changeset/ChangeSetFactory.java
@@ -30,23 +30,24 @@ public final class ChangeSetFactory {
         final Hudson hudson = Hudson.getInstance();
 
         // CVS
-        if (entry instanceof CVSChangeLogSet.CVSChangeLog) {
+        if (hudson.getPlugin("cvs") != null
+                && entry instanceof CVSChangeLogSet.CVSChangeLog) {
             return new CVSChangeSet(id, build, (CVSChangeLogSet.CVSChangeLog) entry);
         }
         // Subversion
-        if (hudson.getPlugin("subversion") != null &&
-             entry instanceof SubversionChangeLogSet.LogEntry) {
-                return new SubversionChangeSet(id, build, (SubversionChangeLogSet.LogEntry) entry);
+        if (hudson.getPlugin("subversion") != null
+                && entry instanceof SubversionChangeLogSet.LogEntry) {
+            return new SubversionChangeSet(id, build, (SubversionChangeLogSet.LogEntry) entry);
         }
         // Mercurial
-        if (hudson.getPlugin("mercurial") != null &&
-            entry instanceof hudson.plugins.mercurial.MercurialChangeSet) {
-                return new MercurialChangeSet(id, build, (hudson.plugins.mercurial.MercurialChangeSet) entry);
+        if (hudson.getPlugin("mercurial") != null
+                && entry instanceof hudson.plugins.mercurial.MercurialChangeSet) {
+            return new MercurialChangeSet(id, build, (hudson.plugins.mercurial.MercurialChangeSet) entry);
         }
         // Git
-        if (hudson.getPlugin("git") != null &&
-            entry instanceof hudson.plugins.git.GitChangeSet) {
-                return new GitChangeSet(id, build, (hudson.plugins.git.GitChangeSet) entry);
+        if (hudson.getPlugin("git") != null
+                && entry instanceof hudson.plugins.git.GitChangeSet) {
+            return new GitChangeSet(id, build, (hudson.plugins.git.GitChangeSet) entry);
         }
         // else
         return new DefaultChangeSet(id, build, entry);
